f6af0527d8913e7da224fe6b5163cb7ddb85a384
apekshhaa <apekshhaa@gmail.com>
2026-06-14 19:35:51 +0530

    add: added files

--- src/scraper.py
+++ src/scraper.py
@@ -1,40 +1,26 @@
 import requests
 import logging
 import time
-
 URL = "https://api.slingacademy.com/v1/sample-data/files/employees.json"
-
 logging.basicConfig(
     level=logging.ERROR,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
-
-
 def fetch_employee_data(retries=3):
-
     for attempt in range(retries):
-
         try:
             response = requests.get(URL, timeout=5)
-
             if response.status_code == 200:
                 return response.json()
-
             logging.error(
                 f"API returned status code {response.status_code}"
             )
-
         except requests.exceptions.Timeout:
             logging.error("Request timed out")
-
         except requests.exceptions.RequestException as e:
             logging.error(f"Request failed: {e}")
-
         time.sleep(2)
-
     raise Exception("Failed after multiple retries")
-
-
 if __name__ == "__main__":
     data = fetch_employee_data()
     print(type(data))
--- src/transformer.py
+++ src/transformer.py
@@ -1,6 +1,4 @@
 import pandas as pd
-
-
 def get_designation(exp):
     if exp < 3:
         return "System Engineer"
@@ -10,25 +8,15 @@ def get_designation(exp):
         return "Senior Data Engineer"
     else:
         return "Lead"
-
-
 def normalize_data(data):
     df = pd.DataFrame(data)
-
-    # Full Name
     df["Full Name"] = df["first_name"] + " " + df["last_name"]
-
-    # Designation
     df["designation"] = df["years_of_experience"].apply(get_designation)
-
-    # Phone validation
     df["phone"] = df["phone"].apply(
         lambda x: "Invalid Number"
         if "x" in str(x)
         else x
     )
-
-    # Data type conversions
     df["age"] = df["age"].astype(int)
     df["years_of_experience"] = df["years_of_experience"].astype(int)
     df["salary"] = df["salary"].astype(int)
