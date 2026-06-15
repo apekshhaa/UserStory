a35811888ea99a35075a8b25c2322cd6f44022c6
apekshhaa <apekshhaa@gmail.com>
2026-06-16 02:01:30 +0530

    feat: add INR to USD salary conversion

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ data = fetch_employee_data()
 
 df = normalize_data(data)
 
-print(df[["Full Name", "phone", "designation"]].head(10))
+print(df[["Full Name", "phone", "designation","salary","salary_usd"]].head(10))
 df.to_csv(
     "output/employees_cleaned.csv",
     index=False
